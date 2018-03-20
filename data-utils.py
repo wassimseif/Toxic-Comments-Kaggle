69782525f652ff4124fd32f16d104463d66ab4d6
Wassim Seifeddine <wassim_seifeddine95@live.com>
2018-03-20 21:07:25 +0200

    Building one hot encoded vectors

--- data-utils.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf 
-import gensim
-class Word2Vec:
-    def load_embeddings(self):
-        model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-        return model
-
-    @staticmethod
-    def
