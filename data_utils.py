ac9a631a03238b8f3bbfa584400ef39cc20d23cc
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2018-03-21 19:08:09 +0200

    WIP

--- data_utils.py
+++ data_utils.py
@@ -20,12 +20,32 @@ def one_hot_encode(label):
 def load_data_set():
 
     df = pd.read_csv('data/train.csv')
-    df.drop(['id'],axis=1)
-    toxic = df['toxic']
+    df.drop(['id'], axis=1)
+
+    df['comment_text'].fillna('unknown')
+    df['toxic'].fillna(0)
+    df['severe_toxic'].fillna(0)
+    df['obscene'].fillna(0)
+    df['threat'].fillna(0)
+    df['insult'].fillna(0)
+
+
+
     comments = df['comment_text']
+    toxic = df['toxic']
     severe_toxic = df['severe_toxic']
     obscene = df['obscene']
     threat = df['threat']
     insult = df['insult']
     identity_hate = df['identity_hate']
-    np.concatenate([toxic, com])
+    labels = np.vstack([toxic, severe_toxic, obscene,threat,insult,identity_hate], axis = 0)
+    print(labels.shape)
+
+
+def main():
+    # My code here
+    load_data_set()
+    pass
+
+if __name__ == "__main__":
+    main()
