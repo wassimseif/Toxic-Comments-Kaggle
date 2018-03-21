4bc783d71ffb0e2f9e29cdba49704eb6b05b9b70
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2018-03-21 18:37:02 +0200

    Working on building the dataset

--- data_utils.py
+++ data_utils.py
@@ -1,10 +1,12 @@
 import tensorflow as tf 
 import gensim
 import pandas as pd 
-import numpy as np 
+import numpy as np
+
 labels = ['toxic','severe_toxic','obscene','threat','insult','identity_hate']
+
 def load_embeddings():
-    model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+    model = gensim.models.KeyedVectors.load_word2vec_format('embeddings/GoogleNews-vectors-negative300.bin', binary=True)
     return model 
 
 def one_hot_encode(label):
@@ -14,3 +16,16 @@ def one_hot_encode(label):
             vector[i] = 1 
     
     return vector
+
+def load_data_set():
+
+    df = pd.read_csv('data/train.csv')
+    df.drop(['id'],axis=1)
+    toxic = df['toxic']
+    comments = df['comment_text']
+    severe_toxic = df['severe_toxic']
+    obscene = df['obscene']
+    threat = df['threat']
+    insult = df['insult']
+    identity_hate = df['identity_hate']
+    np.concatenate([toxic, com])
