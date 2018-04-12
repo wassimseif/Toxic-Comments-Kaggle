cb119611a141e86a42f4cf552b3b8f81ee24b0e9
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2018-04-12 18:52:55 +0300

    Added the Embedding Handler Class

---
+++ EmbeddingHandler.py
@@ -0,0 +1,13 @@
+import gensim
+from gensim.models import Word2Vec
+from tensorflow.contrib.keras.api.keras.preprocessing.text import Tozkenizer
+
+class EmbeddingHandler:
+    def __init__(self):
+        pass
+
+    def read_pretrained_embeddings(self,embedding_file ):
+        google_model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+
+    def train_embeddings(self,sentences):
+        pass
